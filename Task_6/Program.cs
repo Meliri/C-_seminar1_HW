eb576d223fcb33efd15c1193bec7e95d5c402c1f
Lili Sabirova <dusmuhametova1995@mail.ru>
2022-09-18 23:32:54 +0500

    Task 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("Число является четным и делится на 2 без остатка");
+}
+else
+{
+    Console.WriteLine("Число не четное и не делится на 2 без остатка");
+}
+if(number % 2 == 0)
+{
+    Console.WriteLine(number / 2);
+}
