222e375747149cd311995e2d5f2867931cb0d9e9
Lili Sabirova <dusmuhametova1995@mail.ru>
2022-09-18 21:03:26 +0500

    Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,57 @@
+﻿int a = 6;
+int b = 9;
+
+int max = a;
+int min = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+if (a < min) min = a;
+if (b < min) min = b;
+
+Console.Write("min = ");
+Console.WriteLine(min);
+
+
+
+// int a = -12;
+// int b = 3;
+
+// int max = a;
+// int min = a;
+
+// if (a > max) max = a;
+// if (b > max) max = b;
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+// if (a < min) min = a;
+// if (b < min) min = b;
+
+// Console.Write("min = ");
+// Console.WriteLine(min);
+
+
+
+// int a = -7;
+// int b = -14;
+
+// int max = a;
+// int min = a;
+
+// if (a > max) max = a;
+// if (b > max) max = b;
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+// if (a < min) min = a;
+// if (b < min) min = b;
+
+// Console.Write("min = ");
+// Console.WriteLine(min);
