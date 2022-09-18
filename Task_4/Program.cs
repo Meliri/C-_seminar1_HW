51ecfb08b5bc2c4108946166f81767a6eac9e696
Lili Sabirova <dusmuhametova1995@mail.ru>
2022-09-18 21:09:03 +0500

    Task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,42 @@
+﻿int a = 24;
+int b = 15;
+int c = 40;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
+
+// int a = 6;
+// int b = -1;
+// int c = 7;
+
+// int max = a;
+
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+
+
+// int a = -13;
+// int b = -3;
+// int c = -9;
+
+// int max = a;
+
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
